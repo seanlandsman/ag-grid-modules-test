57ab2f54ec1517779fa149489840c29a1577b6a5
Sean Landsman <sean@thelandsmans.com>
2019-04-01 13:04:21 +0100

    AG-2800 Modularise Charts

---
+++ src/charts.ts
@@ -0,0 +1,8 @@
+// import 'ag-grid-community/dist/lib/tooltipModule';
+// import 'ag-grid-enterprise/dist/lib/setFilterModule';
+
+import 'ag-grid-enterprise';
+
+import {SimpleGrid} from "./SimpleGrid";
+
+new SimpleGrid('#myGrid');
